9552489a0acacd67df1e12c05ac9add8ca54cc89
tylercschneider <tylercschneider@gmail.com>
2018-05-15 09:42:38 -0400

    something

--- apiLab.js
+++ apiLab.js
@@ -44,7 +44,7 @@ function doneResponse(data, qty) {
 	for(let i = 0; i<qty; i++) {
 		let title = posts[i].data.title;
 		let image = posts[i].data.url;
-		let link = posts[i].data.thumbnail;
+		let link = posts[i].data.thumbnai8l;
 		let post = new Post(i, title, image, link);
 		lego.push(post);
 		post.addToDom();
